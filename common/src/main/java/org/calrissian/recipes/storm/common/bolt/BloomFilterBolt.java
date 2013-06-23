16030eb99ca69b5d00cb451a701a56fbf78c9801
eawagner <ceraw79@hotmail.com>
2013-06-23 05:21:20 -0400

    Added javadoc, source, and rat plugins to root pom.  Added license headers to all files in codebase

--- common/src/main/java/org/calrissian/recipes/storm/common/bolt/BloomFilterBolt.java
+++ common/src/main/java/org/calrissian/recipes/storm/common/bolt/BloomFilterBolt.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2013 The Calrissian Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.calrissian.recipes.storm.common.bolt;
 
 import backtype.storm.task.OutputCollector;
@@ -8,11 +23,8 @@ import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 import com.google.common.hash.BloomFilter;
-import org.slf4j.Logger;
 
 import java.util.Map;
-
-import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Bolt holds a bloom filter, will output the objects that might be contained by the bloom filter
